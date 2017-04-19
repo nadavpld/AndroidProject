44959e46db5b5d97ba109365b7ca044440915a3d
Nadav Peled <nadavpld@gmail.com>
2017-04-19 15:51:20 +0300

    Some pictures and some more basic code

--- app/src/main/java/com/androidproject/androidproject/MainActivity.java
+++ app/src/main/java/com/androidproject/androidproject/MainActivity.java
@@ -3,6 +3,7 @@ package com.androidproject.androidproject;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,11 +13,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-/*
-        toolbar = (Toolbar)findViewById(R.id.toolbar);
 
-        setSupportActionBar(toolbar);
+    }
 
-        getSupportActionBar().setDisplayShowHomeEnabled(true);*/
+    public void LaunchQuiz(View view) {
+    }
+
+    public void LaunchTakePicture(View view) {
+    }
+
+    public void LaunchRandomPicture(View view) {
+    }
+
+    public void LaunchProfile(View view) {
+    }
+
+    public void LaunchSettings(View view) {
+    }
+
+    public void LaunchBrowsePicture(View view) {
     }
 }
