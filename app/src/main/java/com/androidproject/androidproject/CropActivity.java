88379b703263d208c9603e2a3daaf0fb54b78196
Nadav Peled <nadavpld@gmail.com>
2017-04-30 22:55:26 +0300

    Drawing rectangles on the image

--- app/src/main/java/com/androidproject/androidproject/CropActivity.java
+++ app/src/main/java/com/androidproject/androidproject/CropActivity.java
@@ -1,12 +1,28 @@
 package com.androidproject.androidproject;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Point;
+import android.graphics.RectF;
+import android.graphics.drawable.BitmapDrawable;
 import android.support.design.widget.FloatingActionButton;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+
+import com.theartofdev.edmodo.cropper.CropImage;
+import com.theartofdev.edmodo.cropper.CropImageView;
+
+import java.io.File;
 
 public class CropActivity extends AppCompatActivity {
 
@@ -15,6 +31,10 @@ public class CropActivity extends AppCompatActivity {
     LinearLayout TranslateButton;
 
     FloatingActionButton AddPOIButton;
+
+    File imageFile = null;
+
+    Bitmap bitmap = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +47,15 @@ public class CropActivity extends AppCompatActivity {
 
         AddPOIButton = (FloatingActionButton) findViewById(R.id.AddPOI);
 
+        String path = getIntent().getStringExtra("Uri");
+
+        imageFile = new File(path);
+
+        BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inPreferredConfig = Bitmap.Config.ARGB_8888;
+        bitmap = BitmapFactory.decodeFile(path);
+        Image.setImageBitmap(bitmap);
+
         TranslateButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -37,8 +66,46 @@ public class CropActivity extends AppCompatActivity {
         AddPOIButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                CropImage.activity(FileProvider.getUriForFile(
+                        getApplicationContext(),
+                        getApplicationContext()
+                                .getPackageName() + ".provider", imageFile))
+                        .setGuidelines(CropImageView.Guidelines.ON)
+                        .start(CropActivity.this);
             }
         });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        switch (requestCode) {
+            case CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE:
+                if(resultCode == RESULT_OK) {
+                    CropImage.ActivityResult result = CropImage.getActivityResult(data);
+                    float[] points = result.getCropPoints();
+                    Paint paint = new Paint();
+                    paint.setStyle(Paint.Style.STROKE);
+                    paint.setColor(Color.parseColor("#FFE70B0B"));
+                    paint.setStrokeWidth(5.0f);
+
+                    //Create a new image bitmap and attach a brand new canvas to it
+                    Bitmap tempBitmap = Bitmap.createBitmap(bitmap.getWidth(), bitmap.getHeight(), Bitmap.Config.ARGB_8888);
+                    Canvas tempCanvas = new Canvas(tempBitmap);
+
+                    //Draw the image bitmap into the cavas
+                    tempCanvas.drawBitmap(bitmap, 0, 0, null);
+
+                    //Draw everything else you want into the canvas, in this example a rectangle with rounded edges
+                    tempCanvas.drawRoundRect(new RectF(points[0], points[1], points[4], points[5]), 2, 2, paint);
+
+                    //Attach the canvas to the ImageView
+                    Image.setImageDrawable(new BitmapDrawable(getResources(), tempBitmap));
+
+                    //Saving the updated bitmap
+                    bitmap = tempBitmap;
+                } else {
+
+                }
+        }
     }
 }
